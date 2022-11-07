56f61308a8606449b697efd794a752c4d4ee667a
Anderson Llempen <allempen22@ilg.cat>
2022-11-07 19:40:22 +0100

    Cambios en la rama 2

--- Proyecto/src/git01/TestGit.java
+++ Proyecto/src/git01/TestGit.java
@@ -1,10 +1,14 @@
 package git01;
 
+import java.util.Scanner;
+
 public class TestGit {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		String name = "Bugs Bynny";
 		System.out.print("Hello " + name + "!");
+		scanner.close();
 	}
 
 }
